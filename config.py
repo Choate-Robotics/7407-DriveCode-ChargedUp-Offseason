0ca94ca4f8ae08f8bbf1c9df390bbe62c15823ff
Ann <gmsong1978@gmail.com>
2023-07-09 13:24:03 -0400

    Updated config

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+from units.SI import (
+    inches_to_meters,
+)
+
 driver_centric: bool = True
+
+# Field
+field_length = 651.25 * inches_to_meters
+# field_width = 315.5 * inches_to_meters
+field_width = 8.075
