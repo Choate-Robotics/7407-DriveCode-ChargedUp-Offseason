7af5761840fcb06c5597afd823c797462c0803e9
Ann <gmsong1978@gmail.com>
2023-07-12 13:13:03 -0400

    Cleaned up elevator functions

--- subsystem/elevator.py
+++ subsystem/elevator.py
@@ -1,50 +1,47 @@
-import math
-
 import rev
-import wpilib
 from robotpy_toolkit_7407 import Subsystem
 from robotpy_toolkit_7407.motors.rev_motors import SparkMax, SparkMaxConfig
 
 import config
 import constants
+from units.SI import meters
 
 # TODO: Change config once robot is built
 ELEVATOR_CONFIG = SparkMaxConfig(
     1.5, 0, 0.004, 0.00017, (-1, 1), idle_mode=rev.CANSparkMax.IdleMode.kBrake
 )
 
-class Elevator(Subsystem):
 
-    length_constant: float = 1 / constants.elevator_length_per_rotation
-    angle_constant: float = constants.elevator_rotation_gear_ratio * math.pi / 2
+class Elevator(Subsystem):
+    motor_extend: SparkMax = SparkMax(
+        config.elevator_can_id, config=ELEVATOR_CONFIG, inverted=True
+    )
 
     def __init__(self) -> None:
         super().__init__()
-        self.extend_motor: SparkMax = SparkMax(config.elevator_can_id, config=ELEVATOR_CONFIG, inverted=True)
-
-        self.initialized: bool = False
-        self.position: float = 0.0
 
     def init(self) -> None:
-        self.extend_motor.init()
-        self.initialized = True
+        self.motor_extend.init()
 
     def stop(self) -> None:
-        # Stops the rotation of the elevator motor
-        self.extend_motor.set_raw_output(0)
+        """
+        Stops the elevator
+        """
+        self.motor_extend.set_raw_output(0)
 
-    def set_length(self) -> None:
-        # Sets length in meters
-        self.extend_motor.set_target_position(self.length_constant)
+    def set_length(self, length: meters) -> None:
+        """
+        Sets the length of the elevator in meters
 
-    def get_length(self) -> float:
-        # Returns length in meters
-        return self.extend_motor.get_sensor_position() / self.length_constant;
+        Args:
+            length (meters): Length in meters
+        """
+        self.motor_extend.set_target_position(length / constants.elevator_length_per_rotation)
 
-    def set_angle(self) -> None:
-        # Sets angle in radians
-        self.extend_motor.set_target_position(self.angle_constant)
-
-    def get_angle(self) -> float:
-        # Returns angle in radians
-        return self.extend_motor.get_sensor_position() / self.angle_constant
+    def get_length(self) -> meters:
+        """
+        Gets the length of the elevator
+        :return: Length in meters
+        :rtype: meters
+        """
+        return self.motor_extend.get_sensor_position() * constants.elevator_length_per_rotation
