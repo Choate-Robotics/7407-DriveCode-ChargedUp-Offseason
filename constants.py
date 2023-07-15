b73c47e0c89ce1fe36fd8a23463b5157df213a29
Inspirol <91295122+Inspirol@users.noreply.github.com>
2023-07-15 11:16:23 -0500

    Murgio typo

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ from wpimath.geometry import Pose3d, Translation3d, Rotation3d
 limelight_offset: Pose3d = {
     "front": Pose3d(Translation3d(0.0, 0.0, 0.0), Rotation3d(0.0, 0.0, 0.0)),
     "back": Pose3d(Translation3d(0.0, 0.0, 0.0), Rotation3d(0.0, 0.0, 0.0)),
-
+}
 import math
 
 from robotpy_toolkit_7407.utils.units import hour, m, mile, rad, rev, s
